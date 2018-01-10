f84a5e78b94d63bb3f958d0d3e7c225a23f383ea
Mrbysco <Heeybysco@live.nl>
2018-01-10 23:42:13 +0100

    Reading up for a release
    todo: fix desync of leaves

--- src/main/java/com/Mrbysco/EnhancedFarming/Reference.java
+++ src/main/java/com/Mrbysco/EnhancedFarming/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	public static final String MOD_ID = "enhancedfarming";
 	public static final String MOD_PREFIX = MOD_ID + ":";
 	public static final String MOD_NAME = "Enhanced Farming";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.0.0";
 	public static final String ACCEPTED_VERSIONS = "[1.12]";
 	
 	public static final String CLIENT_PROXY_CLASS = "com.Mrbysco.EnhancedFarming.proxy.ClientProxy";
