f58f077ff788e9bab0a5ddfda88b5fdb5287ca75
Mrbysco <Heeybysco@live.nl>
2018-02-04 03:08:55 +0100

    SQUISH SQUISH SQUISH

--- src/main/java/com/Mrbysco/EnhancedFarming/handler/FarmingHandlers.java
+++ src/main/java/com/Mrbysco/EnhancedFarming/handler/FarmingHandlers.java
@@ -1,19 +1,25 @@
 package com.Mrbysco.EnhancedFarming.handler;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import com.Mrbysco.EnhancedFarming.config.FarmingConfigGen;
 import com.Mrbysco.EnhancedFarming.init.FarmingItems;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.EntityInteract;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -97,6 +103,47 @@ public class FarmingHandlers {
 		
 				this.itemHeld = heldItem;
 			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void InWorldCrafting(TickEvent.PlayerTickEvent event) {
+		if (event.phase.equals(TickEvent.Phase.START) && event.side.isServer())
+		{
+			EntityPlayer player = event.player;
+			World world = player.world;
+			ItemStack oilStack = new ItemStack(FarmingItems.olive_oil);
+
+			List<Entity> entitylist = player.world.loadedEntityList;
+			
+			for (Entity entity : entitylist)
+			{
+				if (entity instanceof EntityItem) {
+					EntityItem itemEntity = (EntityItem) entity;
+					ItemStack stack = itemEntity.getItem();
+					Item item = stack.getItem();
+					
+					if(item == FarmingItems.olive)
+					{
+						BlockPos pos = entity.getPosition();
+						Block block = world.getBlockState(pos).getBlock();
+						Block blockDown = world.getBlockState(pos.down()).getBlock();
+						
+						Double posY = itemEntity.posY;
+						int yInt = posY.intValue();
+						
+						if (block == Blocks.PISTON_HEAD && posY < yInt + 0.25 && blockDown != Blocks.PISTON)
+						{
+							if(!world.isRemote)
+							{
+								oilStack.setCount(stack.getCount());
+								
+								itemEntity.setItem(oilStack);
+							}
+						}
+					}
+				}
+			}	
 		}
 	}
 }
