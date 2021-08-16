beddb580d0fc4f36eba3395e6a9c508f95be53e5
Mrbysco <Mrbysco@users.noreply.github.com>
2021-08-16 15:51:42 +0200

    Fix #10

--- src/main/java/com/mrbysco/enhancedfarming/block/crops/NetherFlowerBlock.java
+++ src/main/java/com/mrbysco/enhancedfarming/block/crops/NetherFlowerBlock.java
@@ -26,7 +26,13 @@ import java.util.Random;
 
 public class NetherFlowerBlock extends BushBlock implements BonemealableBlock{
 
-	private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{Block.box(0.0D, 0.0D, 0.0D, 16.0D, 5.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 11.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D)};
+	private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{
+			Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D),
+			Block.box(0.0D, 0.0D, 0.0D, 16.0D, 3.0D, 16.0D),
+			Block.box(0.0D, 0.0D, 0.0D, 16.0D, 4.0D, 16.0D),
+			Block.box(0.0D, 0.0D, 0.0D, 16.0D, 6.0D, 16.0D),
+			Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D),
+			Block.box(0.0D, 0.0D, 0.0D, 16.0D, 9.0D, 16.0D)};
 	public static final IntegerProperty AGE = BlockStateProperties.AGE_5;
 
     public NetherFlowerBlock(BlockBehaviour.Properties properties) {
@@ -81,8 +87,8 @@ public class NetherFlowerBlock extends BushBlock implements BonemealableBlock{
 		}
 	}
 
-	public VoxelShape getShape(BlockState p_220053_1_, BlockGetter p_220053_2_, BlockPos p_220053_3_, CollisionContext p_220053_4_) {
-		return SHAPE_BY_AGE[p_220053_1_.getValue(AGE)];
+	public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
+		return SHAPE_BY_AGE[state.getValue(AGE)];
 	}
 
 	@Override
