dc761bc7cea0e027205a4264c8f051076cbc8fdb
Mrbysco <Mrbysco@users.noreply.github.com>
2025-08-16 18:30:16 +0200

    Enable in-game config

--- src/main/java/com/mrbysco/enhancedfarming/EnhancedFarming.java
+++ src/main/java/com/mrbysco/enhancedfarming/EnhancedFarming.java
@@ -21,6 +21,8 @@ import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.OnDatapackSyncEvent;
@@ -58,6 +60,7 @@ public class EnhancedFarming {
 		NeoForge.EVENT_BUS.addListener(this::onDatapackSync);
 
 		if (dist.isClient()) {
+			container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
 			eventBus.addListener(ClientHandler::onClientSetup);
 			eventBus.addListener(ClientHandler::registerBlockColors);
 			NeoForge.EVENT_BUS.addListener(ClientHandler::onRecipeReceived);
