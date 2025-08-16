1ff5317abd7189f72d6debcd3eb603dfceeb07fa
Mrbysco <Mrbysco@users.noreply.github.com>
2025-08-16 17:07:19 +0200

    Fix JEI

--- src/main/java/com/mrbysco/enhancedfarming/compat/jei/piston/PistonCategory.java
+++ src/main/java/com/mrbysco/enhancedfarming/compat/jei/piston/PistonCategory.java
@@ -14,10 +14,12 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 
 public class PistonCategory<T extends PistonRecipe> implements IRecipeCategory<PistonRecipe> {
+	private final IDrawable background;
 	private final IDrawable icon;
 	private final Component localizedName;
 
 	public PistonCategory(IGuiHelper guiHelper) {
+		this.background = guiHelper.createDrawable(JEICompat.RECIPE_PISTON_JEI, 0, 0, 84, 62);
 		this.icon = guiHelper.createDrawable(JEICompat.RECIPE_PISTON_ICON_JEI, 0, 0, 16, 16);
 		this.localizedName = Component.translatable("enhancedfarming.gui.jei.category.piston");
 	}
@@ -49,12 +51,13 @@ public class PistonCategory<T extends PistonRecipe> implements IRecipeCategory<P
 
 	@Override
 	public void setRecipe(IRecipeLayoutBuilder builder, PistonRecipe recipe, IFocusGroup focuses) {
-		builder.addSlot(RecipeIngredientRole.INPUT, 1, 23).add(recipe.ingredient()).setStandardSlotBackground();
-		builder.addSlot(RecipeIngredientRole.OUTPUT, 67, 22).add(recipe.result()).setOutputSlotBackground();
+		builder.addSlot(RecipeIngredientRole.INPUT, 1, 23).add(recipe.ingredient());
+		builder.addSlot(RecipeIngredientRole.OUTPUT, 67, 23).add(recipe.result());
 	}
 
 	@Override
 	public void draw(PistonRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
+		this.background.draw(guiGraphics);
 		IRecipeCategory.super.draw(recipe, recipeSlotsView, guiGraphics, mouseX, mouseY);
 	}
 }
