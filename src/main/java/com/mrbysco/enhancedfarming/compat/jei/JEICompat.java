f0af1854dcee6f1494182dbbfc626fa0a342f99e
Mrbysco <Mrbysco@users.noreply.github.com>
2026-08-02 23:27:00 +0200

    Re-enable JEI / REI support

--- src/main/java/com/mrbysco/enhancedfarming/compat/jei/JEICompat.java
+++ src/main/java/com/mrbysco/enhancedfarming/compat/jei/JEICompat.java
@@ -1,59 +1,59 @@
-//package com.mrbysco.enhancedfarming.compat.jei;
-//
-//import com.mrbysco.enhancedfarming.EnhancedFarming;
-//import com.mrbysco.enhancedfarming.compat.jei.piston.PistonCategory;
-//import com.mrbysco.enhancedfarming.recipes.PistonRecipe;
-//import com.mrbysco.enhancedfarming.recipes.PistonRecipeCache;
-//import mezz.jei.api.IModPlugin;
-//import mezz.jei.api.JeiPlugin;
-//import mezz.jei.api.helpers.IGuiHelper;
-//import mezz.jei.api.helpers.IJeiHelpers;
-//import mezz.jei.api.recipe.category.IRecipeCategory;
-//import mezz.jei.api.recipe.types.IRecipeType;
-//import mezz.jei.api.registration.IRecipeCatalystRegistration;
-//import mezz.jei.api.registration.IRecipeCategoryRegistration;
-//import mezz.jei.api.registration.IRecipeRegistration;
-//import net.minecraft.resources.Identifier;
-//import net.minecraft.world.item.ItemStack;
-//import net.minecraft.world.item.Items;
-//import net.minecraft.world.item.crafting.RecipeHolder;
-//import org.jetbrains.annotations.Nullable;
-//
-//@JeiPlugin
-//public class JEICompat implements IModPlugin {
-//	public static final Identifier RECIPE_PISTON_JEI = EnhancedFarming.modLoc("textures/gui/jei/piston.png");
-//	public static final Identifier RECIPE_PISTON_ICON_JEI = EnhancedFarming.modLoc("textures/gui/jei/piston_icon.png");
-//
-//	public static final Identifier PLUGIN_UID = EnhancedFarming.modLoc("main");
-//
-//	public static final IRecipeType<PistonRecipe> PISTON_TYPE = IRecipeType.create(EnhancedFarming.MOD_ID, "piston", PistonRecipe.class);
-//
-//	@Nullable
-//	private IRecipeCategory<PistonRecipe> pistonCategory;
-//
-//	@Override
-//	public Identifier getPluginUid() {
-//		return PLUGIN_UID;
-//	}
-//
-//	@Override
-//	public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
-//		registration.addCraftingStation(PISTON_TYPE, new ItemStack(Items.PISTON));
-//		registration.addCraftingStation(PISTON_TYPE, new ItemStack(Items.STICKY_PISTON));
-//	}
-//
-//	@Override
-//	public void registerCategories(IRecipeCategoryRegistration registration) {
-//		IJeiHelpers jeiHelpers = registration.getJeiHelpers();
-//		IGuiHelper guiHelper = jeiHelpers.getGuiHelper();
-//		registration.addRecipeCategories(
-//				pistonCategory = new PistonCategory<>(guiHelper)
-//		);
-//	}
-//
-//	@Override
-//	public void registerRecipes(IRecipeRegistration registration) {
-//		registration.addRecipes(PISTON_TYPE, PistonRecipeCache.pistonRecipes.stream()
-//				.map(RecipeHolder::value).toList());
-//	}
-//}
+package com.mrbysco.enhancedfarming.compat.jei;
+
+import com.mrbysco.enhancedfarming.EnhancedFarming;
+import com.mrbysco.enhancedfarming.compat.jei.piston.PistonCategory;
+import com.mrbysco.enhancedfarming.recipes.PistonRecipe;
+import com.mrbysco.enhancedfarming.recipes.PistonRecipeCache;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.helpers.IJeiHelpers;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import mezz.jei.api.recipe.types.IRecipeType;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import org.jetbrains.annotations.Nullable;
+
+@JeiPlugin
+public class JEICompat implements IModPlugin {
+	public static final Identifier RECIPE_PISTON_JEI = EnhancedFarming.modLoc("textures/gui/jei/piston.png");
+	public static final Identifier RECIPE_PISTON_ICON_JEI = EnhancedFarming.modLoc("textures/gui/jei/piston_icon.png");
+
+	public static final Identifier PLUGIN_UID = EnhancedFarming.modLoc("main");
+
+	public static final IRecipeType<PistonRecipe> PISTON_TYPE = IRecipeType.create(EnhancedFarming.MOD_ID, "piston", PistonRecipe.class);
+
+	@Nullable
+	private IRecipeCategory<PistonRecipe> pistonCategory;
+
+	@Override
+	public Identifier getPluginUid() {
+		return PLUGIN_UID;
+	}
+
+	@Override
+	public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+		registration.addCraftingStation(PISTON_TYPE, new ItemStack(Items.PISTON));
+		registration.addCraftingStation(PISTON_TYPE, new ItemStack(Items.STICKY_PISTON));
+	}
+
+	@Override
+	public void registerCategories(IRecipeCategoryRegistration registration) {
+		IJeiHelpers jeiHelpers = registration.getJeiHelpers();
+		IGuiHelper guiHelper = jeiHelpers.getGuiHelper();
+		registration.addRecipeCategories(
+				pistonCategory = new PistonCategory<>(guiHelper)
+		);
+	}
+
+	@Override
+	public void registerRecipes(IRecipeRegistration registration) {
+		registration.addRecipes(PISTON_TYPE, PistonRecipeCache.pistonRecipes.stream()
+				.map(RecipeHolder::value).toList());
+	}
+}
--- src/main/java/com/mrbysco/enhancedfarming/compat/rei/display/PistonDisplay.java
+++ src/main/java/com/mrbysco/enhancedfarming/compat/rei/display/PistonDisplay.java
@@ -1,73 +1,73 @@
-//package com.mrbysco.enhancedfarming.compat.rei.display;
-//
-//import com.mojang.serialization.codecs.RecordCodecBuilder;
-//import com.mrbysco.enhancedfarming.compat.rei.FarmingREIPlugin;
-//import com.mrbysco.enhancedfarming.recipes.PistonRecipe;
-//import me.shedaniel.rei.api.common.category.CategoryIdentifier;
-//import me.shedaniel.rei.api.common.display.Display;
-//import me.shedaniel.rei.api.common.display.DisplaySerializer;
-//import me.shedaniel.rei.api.common.entry.EntryIngredient;
-//import me.shedaniel.rei.api.common.util.EntryIngredients;
-//import net.minecraft.network.codec.StreamCodec;
-//import net.minecraft.resources.Identifier;
-//import net.minecraft.world.item.crafting.RecipeHolder;
-//import org.jetbrains.annotations.Nullable;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//public final class PistonDisplay implements Display {
-//	public static final DisplaySerializer<PistonDisplay> SERIALIZER = DisplaySerializer.of(
-//			RecordCodecBuilder.mapCodec(instance -> instance.group(
-//					EntryIngredient.codec().fieldOf("input").forGetter(d -> d.input),
-//					EntryIngredient.codec().fieldOf("output").forGetter(d -> d.output)
-//			).apply(instance, PistonDisplay::new)),
-//			StreamCodec.composite(
-//					EntryIngredient.streamCodec(),
-//					d -> d.input,
-//					EntryIngredient.streamCodec(),
-//					d -> d.output,
-//					PistonDisplay::new
-//			));
-//
-//	private final EntryIngredient input;
-//	private final EntryIngredient output;
-//
-//	public PistonDisplay(EntryIngredient input, EntryIngredient output) {
-//		this.input = input;
-//		this.output = output;
-//	}
-//
-//	public PistonDisplay(RecipeHolder<PistonRecipe> recipeHolder) {
-//		this(
-//				EntryIngredients.ofIngredient(recipeHolder.value().ingredient()),
-//				EntryIngredients.of(recipeHolder.value().result())
-//		);
-//	}
-//
-//	@Override
-//	public List<EntryIngredient> getInputEntries() {
-//		return List.of(input);
-//	}
-//
-//	@Override
-//	public List<EntryIngredient> getOutputEntries() {
-//		return List.of(output);
-//	}
-//
-//	@Override
-//	public CategoryIdentifier<?> getCategoryIdentifier() {
-//		return FarmingREIPlugin.PISTON;
-//	}
-//
-//	@Override
-//	public Optional<Identifier> getDisplayLocation() {
-//		return Optional.empty();
-//	}
-//
-//	@Nullable
-//	@Override
-//	public DisplaySerializer<? extends Display> getSerializer() {
-//		return SERIALIZER;
-//	}
-//}
+package com.mrbysco.enhancedfarming.compat.rei.display;
+
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.mrbysco.enhancedfarming.compat.rei.FarmingREIPlugin;
+import com.mrbysco.enhancedfarming.recipes.PistonRecipe;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.display.Display;
+import me.shedaniel.rei.api.common.display.DisplaySerializer;
+import me.shedaniel.rei.api.common.entry.EntryIngredient;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Optional;
+
+public final class PistonDisplay implements Display {
+	public static final DisplaySerializer<PistonDisplay> SERIALIZER = DisplaySerializer.of(
+			RecordCodecBuilder.mapCodec(instance -> instance.group(
+					EntryIngredient.codec().fieldOf("input").forGetter(d -> d.input),
+					EntryIngredient.codec().fieldOf("output").forGetter(d -> d.output)
+			).apply(instance, PistonDisplay::new)),
+			StreamCodec.composite(
+					EntryIngredient.streamCodec(),
+					d -> d.input,
+					EntryIngredient.streamCodec(),
+					d -> d.output,
+					PistonDisplay::new
+			));
+
+	private final EntryIngredient input;
+	private final EntryIngredient output;
+
+	public PistonDisplay(EntryIngredient input, EntryIngredient output) {
+		this.input = input;
+		this.output = output;
+	}
+
+	public PistonDisplay(RecipeHolder<PistonRecipe> recipeHolder) {
+		this(
+				EntryIngredients.ofIngredient(recipeHolder.value().ingredient()),
+				EntryIngredients.of(recipeHolder.value().result())
+		);
+	}
+
+	@Override
+	public List<EntryIngredient> getInputEntries() {
+		return List.of(input);
+	}
+
+	@Override
+	public List<EntryIngredient> getOutputEntries() {
+		return List.of(output);
+	}
+
+	@Override
+	public CategoryIdentifier<?> getCategoryIdentifier() {
+		return FarmingREIPlugin.PISTON;
+	}
+
+	@Override
+	public Optional<Identifier> getDisplayLocation() {
+		return Optional.empty();
+	}
+
+	@Nullable
+	@Override
+	public DisplaySerializer<? extends Display> getSerializer() {
+		return SERIALIZER;
+	}
+}
